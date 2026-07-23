0d8d091ac41bf8f6f819b53bca5943b8fcdc6f46
GUSTAVO DOS SANTOS SILVA <gustavo_silva209@estudante.sesisenai.org.br>
2026-07-23 14:25:47 -0300

    atividades 06

--- Javascript/aula_06/ex-bonus.js
+++ Javascript/aula_06/ex-bonus.js
@@ -2,7 +2,7 @@ let numeros=[12,5,8,21,3,14,7]
 let maior=0
 let menor=0
 let total=0
-for(let i=0;i<=numeros.length;i++){
+for(let i=0;i<numeros.length;i++){
     total=total+numeros[i]
     if(maior<numeros[i]){
         maior=numeros[i]
